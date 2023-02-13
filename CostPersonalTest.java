609c3d089f746d3787c56967fcc55ca707c29029
IvanPeriche <ivanperiche@iesmontsia.org>
2023-02-13 09:18:28 +0100

    testcost1

--- CostPersonalTest.java
+++ CostPersonalTest.java
@@ -6,41 +6,23 @@ import org.junit.Test;
  * @author IvanPeriche
  * @version 1.0
  */
-    
-    public class CostPersonalTest {
+
+
+ public class CostPersonalTest {
+
         @Test
-        public void testCalculaCostDelPersonal() {
-            Treballador[] treballadors = new Treballador[3];
-            treballadors[0] = new Treballador (null, Treballador.DIRECTOR, 1000, 0);
-            treballadors[1] = new Treballador (null, Treballador.SUBDIRECTOR, 800, 0);
-            treballadors[2] = new Treballador (null, Treballador.BASE, 600, 10);
-            float expected = 1000 + 800 + 600 + (10 * 20);
-            float result = CostPersonal.CalculaCostDelPersonal(treballadors);
-            assertEquals(expected, result, 0);
-        }
-    
-        @Test
-        public void testCostTreballador() {
-            Treballador treballador = new Treballador(null, Treballador.DIRECTOR, 1000, 0);
-            float expected = 1000;
-            float result = CostPersonal.CostTreballador(treballador);
-            assertEquals(expected, result, 0);
-    
-            treballador = new Treballador(null, Treballador.BASE, 600, 10);
-            expected = 600 + (10 * 20);
-            result = CostPersonal.CostTreballador(treballador);
-            assertEquals(expected, result, 0);
-        }
-    
-        @Test
-        public void testCalculaCostDelPersonal2() {
-            Treballador[] treballadors = new Treballador[3];
-            treballadors[0] = new Treballador(null, Treballador.DIRECTOR, 1000, 0);
-            treballadors[1] = new Treballador(null, Treballador.SUBDIRECTOR, 800, 0);
-            treballadors[2] = new Treballador(null, Treballador.BASE, 600, 10);
-            float expected = 1000 + 800 + 600 + (10 * 20);
-            float result = CostPersonal.CalculaCostDelPersonal2(treballadors);
-            assertEquals(expected, result, 0);
-        }
+        public void testCostTreballadorDirector() {
+        Treballador director = new Treballador(Treballador.TipusTreballador.DIRECTOR, 10000f, 0);
+        float cost = CostPersonal.CostTreballador(director);
+        assertEquals(10000f, cost, 0.001f);
+}
+
+
+    @Test
+    public void testCostTreballadorBase() {
+        Treballador base = new Treballador(Treballador.TipusTreballador.BASE, 5000f, 10);
+        float cost = CostPersonal.CostTreballador(base);
+        assertEquals(5000f + 10 * 20, cost, 0.001f);
+    }
     
     }
