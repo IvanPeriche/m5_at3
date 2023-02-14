7c4b6e1808d744d6e5ec6a0cedd2b257bad15e5f
IvanPeriche <ivanperiche@iesmontsia.org>
2023-02-14 12:38:38 +0100

    testcost2

--- CostPersonalTest.java
+++ CostPersonalTest.java
@@ -15,14 +15,12 @@ import org.junit.Test;
         Treballador director = new Treballador(Treballador.TipusTreballador.DIRECTOR, 10000f, 0);
         float cost = CostPersonal.CostTreballador(director);
         assertEquals(10000f, cost, 0.001f);
-}
+        }
 
-
-    @Test
-    public void testCostTreballadorBase() {
+        @Test
+        public void testCostTreballadorBase() {
         Treballador base = new Treballador(Treballador.TipusTreballador.BASE, 5000f, 10);
         float cost = CostPersonal.CostTreballador(base);
         assertEquals(5000f + 10 * 20, cost, 0.001f);
-    }
-    
+        }
     }
